3788b4cb75adbc725b3f0fe98e6deba885e4ae31
Enoch Boison <enoch.boison@amalitech.com>
2023-03-19 17:28:30 +0000

    add delete button fix

--- vue with router/newapp/src/components/state/store.js
+++ vue with router/newapp/src/components/state/store.js
@@ -3,20 +3,13 @@ import { reactive, unref, ref } from "vue";
 export const store = reactive([]);
 
 export let temporaryIndex = ref(0);
-export let taskSelected = reactive([]);
+// export let taskSelected = reactive([]);
 
 export function delItem(index) {
-  const unrefTaskSelected = { ...taskSelected };
+
   if (unref(store).length > 0) {
-    // console.log(unrefTaskSelected)
-    if (unrefTaskSelected[0].selected) {
-      if (unrefTaskSelected[0].id === index) {
-        unref(store).splice(index, 1);
-      } else {
-        alert("please select to delete");
+    unref(store).splice(index, 1);
       }
-    } else {
-      alert("No task selected!");
-    }
-  }
-}
+    } 
+  
+
--- vue with router/newapp/src/components/views/ToDoEditor.vue
+++ vue with router/newapp/src/components/views/ToDoEditor.vue
@@ -35,7 +35,7 @@
             type="date"
             name="date"
             id="first-name"
-            class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            class="hover:cursor-not-allowed block w-full rounded-md border-0 py-2 px-3.5 text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
         </div>
       </div>
@@ -47,7 +47,7 @@
             type="time"
             name="time"
             id="last-name"
-            class="block w-full rounded-md border-0 py-2 px-3.5 text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            class="hover:cursor-not-allowed block w-full rounded-md border-0 py-2 px-3.5 text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
         </div>
       </div>
